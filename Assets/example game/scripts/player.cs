bc32831abebb897ca966db4f26aaf2c8e0136bf1
BrandonsAFatCunt <36157016+BrandonsAFatCunt@users.noreply.github.com>
2018-02-12 12:57:21 +0000

    Added code

---
+++ Assets/example game/scripts/player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello asshole!");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
