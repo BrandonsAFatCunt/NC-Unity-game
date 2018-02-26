da937f1cd204c1fd01cc26a7875ecb48d03ec52b
BrandonsAFatCunt <36157016+BrandonsAFatCunt@users.noreply.github.com>
2018-02-26 11:24:40 +0000

    Example player code

--- Assets/example game/scripts/player.cs
+++ Assets/example game/scripts/player.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello asshole!");
-	
+   public string logtext = "I don't understand how people find this is confusing";
+   public float speed = 2;
+    // Use this for initialization
+    void Start () {
+        
+        Debug.Log(logtext);
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+       
+        rigidbody.velocity = Vector2.right * speed;	
 	}
 	
 	// Update is called once per frame
