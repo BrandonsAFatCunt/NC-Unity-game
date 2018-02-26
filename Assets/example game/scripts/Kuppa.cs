bb761d743ded8bd97bbd5b153b88b13b43a8f237
BrandonsAFatCunt <36157016+BrandonsAFatCunt@users.noreply.github.com>
2018-02-26 12:52:38 +0000

    Added enimes and damage

---
+++ Assets/example game/scripts/Kuppa.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kuppa : MonoBehaviour {
+    public float damage = 2;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision!");
+        
+        player mario = collision.collider.GetComponent<player>();
+        mario.apply_damage(damage);
+    }
+}
--- Assets/example game/scripts/player.cs
+++ Assets/example game/scripts/player.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class player : MonoBehaviour {
    public string logtext = "I don't understand how people find this is confusing";
    public float speed = 2;
+    public float health = 10;
     // Use this for initialization
     void Start () {
         
         Debug.Log(logtext);
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-       
-        rigidbody.velocity = Vector2.right * speed;	
-	}
+        //apply_damage(1);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+       
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = Vector2.right * speed;
+
+    }
+   public void apply_damage(float damage)
+    {
+        health = health - damage;
+     
+ }  
 }
