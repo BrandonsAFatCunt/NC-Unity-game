909ee5bc669400b3b7feec1eef8c10dc2a576701
BrandonsAFatCunt <36157016+BrandonsAFatCunt@users.noreply.github.com>
2018-03-05 10:49:58 +0000

    Added movment and jump

--- Assets/example game/scripts/player.cs
+++ Assets/example game/scripts/player.cs
@@ -5,6 +5,7 @@ public class player : MonoBehaviour {
    public string logtext = "I don't understand how people find this is confusing";
    public float speed = 2;
     public float health = 10;
+    public float jumpspeed = 50;
     // Use this for initialization
     void Start () {
         
@@ -17,8 +18,32 @@ public class player : MonoBehaviour {
        
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
-        rigidbody.velocity = Vector2.right * speed;
+       
+        // number between -1 and 1 based on player pressing left or right
+        float horizontal = Input.GetAxis("Horizontal");
 
+        bool jump = Input.GetButtonDown("Jump");
+
+        Collider2D collider =GetComponent<Collider2D>();
+        LayerMask groundlayer = LayerMask.GetMask("Ground");
+        bool touchingGround = collider.IsTouchingLayers(groundlayer);
+
+        Debug.Log(horizontal);
+
+        Vector2 velocity = rigidbody.velocity;
+        velocity.x = horizontal *speed;
+        if (jump==true && touchingGround==true)
+        {
+            velocity.y = jumpspeed;
+        }
+        rigidbody.velocity = velocity;
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log(" mouse button down!");
+        }
+
+        Vector2 mouseposition = Input.mousePosition;
+        Debug.Log("Mouse position is" + mouseposition);
     }
    public void apply_damage(float damage)
     {
